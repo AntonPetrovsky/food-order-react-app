3bfd066316cc7bc21e4ddd9bed003fdb4ca08a92
Anton Piatroski <anton.piatroski@crealogix.com>
2022-06-13 17:27:37 +0300

    Display Cart items amount

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,14 +1,25 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "./CartIcon";
 import classes from "./CartButton.module.css";
 
-const CartButton = (props) => (
-  <button className={classes.button} onClick={props.showCart}>
-    <span className={classes.icon}>
-      <CartIcon />
-    </span>
-    <span>Your Cart</span>
-    <span className={classes.badge}>0</span>
-  </button>
-);
+const CartButton = (props) => {
+  const cartContext = useContext(CartContext);
+
+  const cartItemsAmount = cartContext.cartItems.reduce(
+    (accumulator, item) => accumulator + item.amount,
+    0
+  );
+
+  return (
+    <button className={classes.button} onClick={props.showCart}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{cartItemsAmount}</span>
+    </button>
+  );
+};
 
 export default CartButton;
